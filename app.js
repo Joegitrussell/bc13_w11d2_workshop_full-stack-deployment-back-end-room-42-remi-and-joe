bf827b6627e60039064b6f0f343e1d5f85c83d38
Joegitrussell <112443220+Joegitrussell@users.noreply.github.com>
2022-12-06 11:19:47 +0000

    setup

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
-
+//const PORT = process.env.PORT;
 import shoppingListRouter from "./routes/shoppingList.js";
 
 const app = express();
@@ -12,5 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/items", shoppingListRouter);
+
+
+//app.listen(PORT, function () {
+  // console.log(`Server is listening on http://localhost:${PORT}`);
+// });
 
 export default app;
